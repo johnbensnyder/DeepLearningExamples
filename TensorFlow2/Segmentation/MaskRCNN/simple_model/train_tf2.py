abf8a6b97a767f56b2f6415e60730d53af54f4f7
johnbensnyder <johnbensnyder@gmail.com>
2020-09-24 22:26:58 +0000

    hpo

--- TensorFlow2/Segmentation/MaskRCNN/simple_model/train_tf2.py
+++ TensorFlow2/Segmentation/MaskRCNN/simple_model/train_tf2.py
@@ -133,17 +133,17 @@ data_params['batch_size'] = batch_size
 params['finetune_bn'] = False
 params['train_batch_size'] = batch_size
 params['l2_weight_decay'] = 1e-4
-params['init_learning_rate'] = 1e-2 * global_batch_size / 8
-params['warmup_learning_rate'] = 1e-4 * global_batch_size / 8
-params['warmup_steps'] = 512
+params['init_learning_rate'] = 15e-3 * global_batch_size / 8
+params['warmup_learning_rate'] = 15e-4 * global_batch_size / 8
+params['warmup_steps'] = steps_per_epoch
 params['learning_rate_steps'] = [steps_per_epoch * 9, steps_per_epoch * 11]
-params['learning_rate_levels'] = [1e-3 * global_batch_size / 8, 1e-4 * global_batch_size / 8]
+params['learning_rate_levels'] = [15e-4 * global_batch_size / 8, 15e-5 * global_batch_size / 8]
 params['momentum'] = 0.9
 params['use_batched_nms'] = False
 params['use_custom_box_proposals_op'] = True
 params['amp'] = True
 params['include_groundtruth_in_features'] = True
-
+params['gradient_clip'] = 3
 loader = dataset_utils.FastDataLoader(train_file_pattern, data_params)
 train_tdf = loader(data_params)
 train_tdf = train_tdf.apply(tf.data.experimental.prefetch_to_device(devices[0].name, 
@@ -188,6 +188,8 @@ def train_step(features, labels, params, model, opt, first=False):
     tape = hvd.DistributedGradientTape(tape)
     scaled_gradients = tape.gradient(scaled_loss, model.trainable_variables)
     gradients = optimizer.get_unscaled_gradients(scaled_gradients)
+    clipped_grads, global_norm = tf.clip_by_global_norm(gradients, params['gradient_clip'])
+    gradients = clipped_grads
     optimizer.apply_gradients(zip(gradients, model.trainable_variables))
     if first:
         hvd.broadcast_variables(model.variables, 0)
