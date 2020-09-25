729ad73dac9c897b0c314d822fae02bd0d108ed5
johnbensnyder <johnbensnyder@gmail.com>
2020-09-25 02:39:44 +0000

    eval fix

--- TensorFlow2/Segmentation/MaskRCNN/simple_model/train_tf2.py
+++ TensorFlow2/Segmentation/MaskRCNN/simple_model/train_tf2.py
@@ -82,7 +82,7 @@ def do_eval(worker_predictions):
     for k, v in _preds.items():
         # combined all results in flat structure for eval
         _preds[k] = np.concatenate(v, axis=0)
-    if MPI_rank() < 32:
+    if MPI_rank() < 512:
         converted_predictions = coco.load_predictions(_preds, include_mask=True, is_image_mask=False)
         worker_source_ids = _preds['source_id']
     else:
@@ -107,10 +107,10 @@ def do_eval(worker_predictions):
         all_predictions = []
         source_ids = []
         for i, p in enumerate(predictions_list):
-            if i < 32: # max eval workers (TODO config)
+            if i < 512: # max eval workers (TODO config)
                 all_predictions.extend(p)
         for i, s in enumerate(source_ids_list):
-            if i < 32:
+            if i < 512:
                 source_ids.extend(s)
 
         # run metric calculation on root node TODO: launch this in it's own thread
